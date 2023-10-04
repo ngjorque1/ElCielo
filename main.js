06a23dab66b60d22a2dda884b45f6e4828883a7e
ngjorque1 <jorqueraw19@gmail.com>
2023-10-03 23:11:30 -0300

    Segunda pre entrega

--- main.js
+++ main.js
@@ -1,37 +1,79 @@
+const carrito = []
 
-alert("Bienvenidos a El Cielo");
+const productos = [
+    {sku: 290930,
+     nombre: "Pilsen de sur",
+     categoria: "Cerveza",
+     precio: 1200,
+     cantidad: 0,},
 
-let producto = prompt("¿Quieres comprar Agua o Vino?");
+     {sku: 300930,
+     nombre: "Corona",
+     categoria: "Cerveza",
+     precio: 2000,
+     cantidad: 0,},
 
-producto = producto.toLowerCase();
+     {sku: 310930,
+     nombre: "Baltica",
+     categoria: "Cerveza",
+     precio: 800,
+     cantidad: 0,},
 
-if (producto == "vino") {
-    let edad = prompt("Por favor, ingrese su edad:");
-    
-if ( edad < 18) {
-        alert("Lo siento, debe ser mayor de edad para comprar Vino.");
+     {sku: 320930,
+     nombre: "Red Label",
+     categoria: "Whiskey",
+     precio: 20000,
+     cantidad: 0,},
+
+     {sku: 330930,
+     nombre: "Black Label",
+     categoria: "Whiskey",
+     precio: 50000,
+     cantidad: 0,},
+
+     {sku: 340930,
+     nombre: "Blue Label",
+     categoria: "Whiskey",
+     precio: 100000,
+     cantidad: 0,},]
+
+let categoriaBuscada = prompt("Busca Cerveza o Whiskey?");
+if (categoriaBuscada.toLowerCase() === "cerveza" || categoriaBuscada.toLowerCase() === "whiskey") {
+    const productosFiltrados = productos.filter(producto => producto.categoria.toLowerCase() == categoriaBuscada.toLowerCase());
+
+    if (productosFiltrados.length > 0) {
+        const mensaje = productosFiltrados.map(producto => `${producto.nombre} - $${producto.precio}`).join('\n');
+        const productoElegido = prompt("¿Cual desea comprar?:\n" + mensaje);
+
+        const productoEncontrado = productosFiltrados.find(producto => producto.nombre.toLowerCase() === productoElegido.toLowerCase());
+
+
+        if (productoEncontrado) {
+            const cantidad = parseInt(prompt(`¿Cuántos ${productoEncontrado.nombre} desea comprar?`));
+            if (!isNaN(cantidad) && cantidad > 0) {
+                carrito.push({
+                    nombre: productoEncontrado.nombre,
+                    precio: productoEncontrado.precio,
+                    cantidad: cantidad
+                });
+                alert(`${cantidad} ${productoEncontrado.nombre} se ha(n) agregado al carrito.`);
+            } else {
+                alert("La cantidad ingresada no es válida.");
+            }
+        } else {
+            alert("El producto seleccionado no se encontró en la lista.");
+        }
     } else {
-        alert("Favor recuerde beber moderadamente y de forma responsable");
+        alert("No se encontraron productos en la categoría buscada.");
     }
-} else if (producto == "agua") {
-    alert("Favor considerar una espera de despacho de 5 días hábiles debido a la escasez.");
 } else {
-    alert("Favor seleccione una de las opciones válidas (Agua o Vino).");
+    alert("Favor ingresar una de las categorías mencionadas.");
 }
 
+console.log("Carrito de compras:");
+console.log(carrito);
 
-let cantidad;
 
-do {
-    cantidad = (prompt("¿Cuántas unidades necesita?"));
-    if (cantidad <= 0) {
-        alert("Favor ingresar una cantidad válida");
-    } else if (cantidad > 5) {
-        alert("Favor considerar que solo puede comprar 5 unidades como máximo.");
-    }
-} while (cantidad > 5);
-
-alert("Su pedido será procesado, ¡Gracias por su compra!");
 
 
 
